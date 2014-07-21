84c5eb98758931588a080c557ea99adc55317ed6
Chris Zamierowski <chriszamierowski@gmail.com>
2014-07-21 16:24:44 -0500

    [Completes #75061514] expanded morsel layout/carousel

--- src/common/modalItems/morsel/modalMorsel.js
+++ src/common/modalItems/morsel/modalMorsel.js
@@ -9,7 +9,10 @@ angular.module( 'Morsel.common.modal.morsel', [] )
     },
     replace: true,
     link: function(scope, element, attrs) {
-      var tempMorsel;
+      var tempMorsel,
+          morselPlaceholderUrl = '/assets/images/util/morsel-placeholder_480x480.jpg',
+          transformProperty,
+          morselHeight = 390;
       
       $http.get('../../assets/cache/morsels/'+scope.id+'.json').success(function(resp){
         tempMorsel = resp.data;
@@ -19,6 +22,47 @@ angular.module( 'Morsel.common.modal.morsel', [] )
 
       scope.$on('modal.morsel.displayData', function() {
         scope.morsel = tempMorsel;
+      });
+
+      scope.formatDescription = function(description) {
+        if(description) {
+          return description.replace(/(\r\n|\n|\r)/g,"<br />");
+        } else {
+          return '';
+        }
+      };
+
+      scope.getItemPhoto = function(item) {
+        if(item.photos) {
+          return item.photos._480x480;
+        } else {
+          return morselPlaceholderUrl;
+        }
+      };
+
+      scope.getUserPhoto = function(user) {
+        if(user.photos) {
+          return user.photos._40x40;
+        } else {
+          return userPlaceholderUrl;
+        }
+      };
+
+      scope.next = function(itemNum) {
+        element.children(1).css(transformProperty, 'translate(0, -' + ((itemNum + 1) * morselHeight) + 'px)');
+      };
+
+      scope.prev = function(itemNum) {
+        element.children(1).css(transformProperty, 'translate(0, -' + ((itemNum - 1) * morselHeight) + 'px)');
+      };
+
+      ['webkit', 'Moz', 'O', 'ms'].every(function (prefix) {
+        var e = prefix + 'Transform';
+        if (typeof document.body.style[e] !== 'undefined') {
+          transformProperty = e;
+          return false;
+        }
+        return true;
       });
     },
     templateUrl: 'modalItems/morsel/modalMorsel.tpl.html'
