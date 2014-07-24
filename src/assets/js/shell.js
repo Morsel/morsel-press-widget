89c9cc80377d38349e66c49e300ef3f9797ef9ae
Chris Zamierowski <chriszamierowski@gmail.com>
2014-07-24 17:40:51 -0500

    [Completes #75061306] handle query params in shell

--- src/assets/js/shell.js
+++ src/assets/js/shell.js
@@ -1,22 +1,45 @@
 ;(function(){
   'use strict';
-  var siteDomain = window.morselConfig ? window.morselConfig.siteDomain : 'http://morsel-media.herokuapp.com';
+  var siteDomain = window.morselConfig ? window.morselConfig.siteDomain : 'http://morsel-media.herokuapp.com',
+    mrslType,
+    mrslId,
+    chan;
 
-  iFrameResize({heightCalculationMethod:'max'});
-
-  var chan = Channel.build({
-    window: document.getElementById("morsel-iframe").contentWindow,
-    origin: siteDomain,
-    scope: "mrsl.pressKit",
-    onReady: fireOpenModal
+  iFrameResize({
+    heightCalculationMethod:'max',
+    initCallback: checkHash
   });
+
+  function checkHash() {
+    var hash = decodeURIComponent(window.location.hash),
+        typeRegExp = /[#&]mrsltype=([^&#]*)/,
+        typeMatches = hash.match(typeRegExp),
+        idRegExp = /[&#]mrslid=([^&#]*)/,
+        idMatches = hash.match(idRegExp);
+
+    if(typeMatches && idMatches) {
+      mrslType = typeMatches[1];
+      mrslId = idMatches[1];
+
+      openChannel();
+    }
+  }
+
+  function openChannel() {
+    chan = Channel.build({
+      window: document.getElementById("morsel-iframe").contentWindow,
+      origin: siteDomain,
+      scope: "mrsl.pressKit",
+      onReady: fireOpenModal
+    });
+  }
 
   function fireOpenModal() {
     chan.call({
       method: "openModal",
       params: {
-        type: 'morsel',
-        id: 421
+        type: mrslType,
+        id: mrslId
       },
       success: function(v) {
         console.log(v);
