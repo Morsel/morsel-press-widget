54b8d80445db5c4a35d1f8e2eb7cd480577be609
Chris Zamierowski <chriszamierowski@gmail.com>
2014-08-05 17:22:31 -0500

    social link fallbacks

--- src/common/morsel/sharing.js
+++ src/common/morsel/sharing.js
@@ -36,24 +36,33 @@ angular.module( 'Morsel.common.morselSharing', [] )
             shareText,
             s = scope.morsel,
             //use their handle if they have one - otherwise use their name
-            twitterUsername = s.creator.twitter_username ? '@'+s.creator.twitter_username : s.creator.first_name+' '+s.creator.last_name;
+            twitterUsername = s.creator.twitter_username ? '@'+s.creator.twitter_username : s.creator.first_name+' '+s.creator.last_name,
+            //backup
+            mediaUrl = 'http://media.eatmorsel.com/morsels/'+s.id,
+            facebookUrl = s.mrsl && s.mrsl.facebook_media_mrsl ? s.mrsl.facebook_media_mrsl : mediaUrl,
+            twitterUrl = s.mrsl && s.mrsl.twitter_media_mrsl ? s.mrsl.twitter_media_mrsl : mediaUrl,
+            linkedinUrl = s.mrsl && s.mrsl.linkedin_media_mrsl ? s.mrsl.linkedin_media_mrsl : mediaUrl,
+            pinterestUrl = s.mrsl && s.mrsl.pinterest_media_mrsl ? s.mrsl.pinterest_media_mrsl : mediaUrl,
+            googleplusUrl = s.mrsl && s.mrsl.googleplus_media_mrsl ? s.mrsl.googleplus_media_mrsl : mediaUrl,
+            clipboardUrl = s.mrsl && s.mrsl.clipboard_media_mrsl ? s.mrsl.clipboard_media_mrsl : mediaUrl;
 
         //come back to updating share links properly
 
         if(socialType === 'facebook') {
-          url = 'https://www.facebook.com/sharer/sharer.php?u='+s.mrsl.facebook_media_mrsl;
+          url = 'https://www.facebook.com/sharer/sharer.php?u='+facebookUrl;
         } else if(socialType === 'twitter') {
           shareText = encodeURIComponent('"'+s.title+'" from '+twitterUsername+' on @eatmorsel ');
-          url = 'https://twitter.com/home?status='+shareText+s.mrsl.twitter_media_mrsl;
+          url = 'https://twitter.com/home?status='+twitterUrl;
         } else if(socialType === 'linkedin') {
-          url = 'https://www.linkedin.com/shareArticle?mini=true&url='+s.mrsl.linkedin_media_mrsl;
+          url = 'https://www.linkedin.com/shareArticle?mini=true&url='+linkedinUrl;
         } else if(socialType === 'pinterest') {
           shareText = encodeURIComponent('"'+s.title+'" from '+s.creator.first_name+' '+s.creator.last_name+' on Morsel');
-          url = 'https://pinterest.com/pin/create/button/?url='+s.mrsl.pinterest_media_mrsl+'&media='+encodeURIComponent(getMediaImage())+'&description='+shareText;
-        } else if(socialType === 'google_plus') {
-          url = 'https://plus.google.com/share?url='+s.mrsl.googleplus_media_mrsl;
+          url = 'https://pinterest.com/pin/create/button/?url='+pinterestUrl+'&media='+encodeURIComponent(getMediaImage())+'&description='+shareText;
+        } else if(socialType === 'googleplus') {
+          url = 'https://plus.google.com/share?url='+googleplusUrl;
         } else if(socialType === 'clipboard') {
-          window.prompt("Copy the following link to share:", s.mrsl.clipboard_mrsl);
+          window.prompt("Copy the following link to share:", clipboardUrl);
+          return;
         }
 
         $window.open(url);
